9cbe2afded7c7b98d66416f35c2764bfadca3147
myattasaurus <rcoins84@yahoo.com>
2023-12-05 20:43:01 -0500

    style

--- scripts.js
+++ scripts.js
@@ -28,7 +28,7 @@ function createTaskEntry() {
     let input = document.createElement('input');
     input.setAttribute('id', 'taskEntry');
     input.setAttribute('type', 'text');
-    input.setAttribute('class', 'w-100 p-2 border-0 text-center');
+    // input.setAttribute('class', 'w-100 p-2 border-0 text-center');
     input.onblur = function (event) {
         findTaskEntry().focus();
     };
@@ -53,7 +53,7 @@ function findTask() {
 function createTask(taskEntryValue) {
     let para = document.createElement('p');
     para.setAttribute('id', 'task');
-    para.appendChild(text(taskEntryValue));
+    para.appendChild(document.createTextNode(taskEntryValue));
 
     document.getElementById('dontainer').appendChild(para);
 }
@@ -67,8 +67,4 @@ function removeTaskAndCreateTaskEntry() {
         findTask().remove();
         createTaskEntry();
     }, 400);
-}
-
-function text(str) {
-    return document.createTextNode(str);
 }
