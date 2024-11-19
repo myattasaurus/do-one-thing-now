44dc1ca3967895de456eb7279c85f5c3401d7147
myattasaurus <rcoins84@yahoo.com>
2024-11-18 23:35:48 -0500

    keyup test mobile

---
+++ keyup.js
@@ -0,0 +1,6 @@
+window.onkeyup = (event) => {
+    document.getElementById('keyup').innerHTML = event.key;
+}
+window.onkeydown = (event) => {
+    document.getElementById('keydown').innerHTML = event.key;
+}
